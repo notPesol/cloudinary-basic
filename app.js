34edc45c3769906939bcf411ac32cd8f6148f0b0
notPesol <pesol2012@hotmail.com>
2021-09-02 14:12:33 +0700

    add remove image func

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const cloudinary = require('cloudinary').v2;
 
-const CLOUD_BASE_URL= process.env.CLOUD_BASE_URL;
+const CLOUD_BASE_URL = process.env.CLOUD_BASE_URL;
 const CLOUD_NAME = process.env.CLOUD_NAME;
 const CLOUD_API_KEY = process.env.CLOUD_API_KEY;
 const CLOUD_API_SECRET = process.env.CLOUD_API_SECRET;
@@ -20,9 +20,8 @@ const PORT = process.env.PORT || 3000;
 
 const formidable = require('formidable');
 
-const uploadDir = path.join(__dirname, '/public', '/images');
-
 const fs = require('fs');
+const os = require('os');
 
 const mongoose = require('mongoose');
 
@@ -44,42 +43,38 @@ app.get('/', async (req, res, next) => {
   try {
     const images = await Image.find({});
     res.render('images', { images, baseUrl: CLOUD_BASE_URL, imgSetting: 'c_fill,w_500,h_500' });
-    console.log(images);
   } catch (error) {
     next(error);
   }
-
-});
-
-app.get('/upload', (req, res) => {
-  res.render('upload')
 });
 
 app.post('/upload', (req, res, next) => {
-  const form = formidable({ uploadDir });
+  const form = formidable();
   form.parse(req, async (err, fields, files) => {
     if (err) return next(err);
 
     const file = files.upfile;
 
     const isValid = isFileValid(file);
-    if(!isValid) return next(new Error('File type error'));
+    if (!isValid) return next(new Error('File type error'));
+
+    console.log(file);
 
     // create a file name
     const fileName = encodeURIComponent(file.name.replace(/\s/g, "-"));
-    const fullPathFile = path.join(uploadDir, fileName);
+    const oldName = file.path;
+    const newName = path.join(os.tmpdir(), fileName);
 
     try {
-      fs.renameSync(file.path, fullPathFile);
-      const img = await cloudinary.uploader.upload(fullPathFile, {
+      fs.renameSync(oldName, newName);
+      const img = await cloudinary.uploader.upload(newName, {
         folder: "basic",
         unique_filename: true,
         use_filename: true
       });
-      console.log(img);
       const image = new Image({
         url: img.public_id
-      })
+      });
       await image.save();
       console.log(image);
       res.redirect('/');
@@ -89,9 +84,26 @@ app.post('/upload', (req, res, next) => {
   })
 });
 
+app.post('/:imgId', async (req, res, next) => {
+  try {
+    const { imgId } = req.params;
+    const img = await Image.findByIdAndDelete(imgId);
+    console.log(img);
+    cloudinary.uploader.destroy(img.url, {
+      type: 'upload'
+    }, (err, result) => {
+      if (err) next(err)
+      res.redirect('/');
+      console.log(result);
+    });
+  } catch (error) {
+    next(error)
+  }
+})
+
 app.use((err, req, res, next) => {
   res.json({ err });
-})
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`);
